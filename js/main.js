e6a035be8f7ad80f50f3a2e0d9c0486599a497a6
theMO <mecocci15@gmail.com>
2018-09-20 23:47:58 +0200

    fixed scroll aniamtions

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 let mobileMenu = document.getElementById("mobileMenu");
 let mobileBtn = document.getElementById("mobileBtn");
+let navLinks = document.getElementsByClassName("header__nav")[0].children;
+let bigBtn = document.getElementById("bigBtn");
+let about = document.getElementById("about");
 
 mobileBtn.addEventListener("click", function() {
   this.classList.toggle("fa-bars");
@@ -7,4 +10,21 @@ mobileBtn.addEventListener("click", function() {
 
   mobileMenu.classList.toggle("mobile");
   document.documentElement.classList.toggle("stop");
+});
+
+Array.from(navLinks).forEach(function(link) {
+  link.addEventListener("click", function() {
+    const to = this.dataset.to;
+    const elem = document.getElementById(to);
+
+    elem.scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
+
+bigBtn.addEventListener("click", () => {
+  about.scrollIntoView({
+    behavior: "smooth"
+  });
 });
